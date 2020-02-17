e8c49ced6071b15aa5a315fe425b43b6e7f78f98
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-02-17 13:13:09 -0800

    created collections items and page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Switch, Route} from 'react-router-dom'
 import './App.css';
-import { Homepage } from './pages/homepage'
+import { Homepage } from './pages/homepage/homepage.component.jsx'
+import ShopPage from './pages/shop/shop.component.jsx'
 
 
-const HatsPage = (props) => {
-  console.log(props)
- return (<div>
-    <h1>Hats Page</h1>
-  </div>)
-}
+// const HatsPage = (props) => {
+//   console.log(props)
+//  return (<div>
+//     <h1>Hats Page</h1>
+//   </div>)
+// }
 
 function App() {
   return (
     <div >
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route path='/hats/:jhj' component={HatsPage} />
+        <Route  path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
