576633b65f0c6872c6827b640de860a6e9153f93
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-02-23 15:54:34 -0800

    added shop nested routes by category

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,6 +4,8 @@ import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
+
+//Route gives you match , location and history bc shoppage is nested in a route
 const ShopPage = ({ match }) => (
   <div className='shop-page'>
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
