b4cb41793002eb032d2da6fd1fb4fbfa89488805
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-02-23 15:21:21 -0800

    added persist redux aka cookies to remember items in cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
+import {
+    persistStore
+} from 'redux-persist';
+
 import logger from 'redux-logger';
+
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+// presist allows to cache the store in local browser 
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
