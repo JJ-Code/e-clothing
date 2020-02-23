083030d5bfb9a433bdbdcae01a17a0fe5625509e
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-02-23 14:43:48 -0800

    added checkout page

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import CustomButton from "../custom-button/custom-button.component";
 import { connect } from "react-redux";
-import { addItem } from "../../redux/cart/cart.action";
+import { addItem } from "../../redux/cart/cart.actions";
 import './collection-item.styles.scss';
 
 const CollectionItem = ({item, addItem }) => {
