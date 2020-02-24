3412e476078b3c88bfa857fbc21e82e87cefb088
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-02-23 16:47:28 -0800

    add stripe payments

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  //stripe wants everything in cents 
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_mZYwlK6BlJztR8Q50JJyGUuI00OPpABqhh';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='JJ Styles Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -9,6 +9,9 @@ import CollectionPage from '../collection/collection.component';
 const ShopPage = ({ match }) => (
   <div className='shop-page'>
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
+
+    {/* since CollectionPage is nested in Route it has access to match so it can chain 
+    using : path of CollectionPage */}
     <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
