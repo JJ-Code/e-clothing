5d6b3cb84ba2708f53c64c8aaed9efcd729b4ccd
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-02-23 17:19:11 -0800

    remove middleware logger from production

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,6 +9,10 @@ import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
+//only works in dev and not production
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 // presist allows to cache the store in local browser 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
